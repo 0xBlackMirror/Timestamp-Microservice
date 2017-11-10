693b79dc278f13cf9faa040bafc47e2fcad7ea0b
WackThat <martin50679@gmail.com>
2017-11-10 22:40:09 +0200

    Complete Unix converter

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const app = express();
+
+app.get('/:t', (req, res) => {
+    console.log(req.params.t);
+    if(!isNaN(req.params.t)){
+        // Unix Time Into Readable
+        //console.log('Unix Error? : ' + naturalDate.toDateString());
+        let naturalDate = new Date(req.params.t *1000);
+        var dateFormattingOptions = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }
+        res.json({
+            natural: naturalDate.toLocaleDateString('en-us', dateFormattingOptions),
+            UNIX: req.params.t
+        });
+    } else {
+        // Natural Date Into Unix
+        let unixDate = new Date(req.params.t);
+        let myEpoch = unixDate.getTime()/1000.0;
+        res.json({
+            natural: req.params.t,
+            UNIX: myEpoch
+        });
+        }
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
+
